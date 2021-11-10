24c1b4ef8847dfc1f5005601887b3dfe0ee15087
zyuezheng <zyuezheng@gmail.com>
2021-11-11 02:38:51 +0800

    added utils

---
+++ utils/dnsutils.sh
@@ -0,0 +1,1 @@
+kubectl run dnsutils --image=gcr.io/kubernetes-e2e-test-images/dnsutils:1.3 --restart=Never --command sleep 10000000
---
+++ utils/run.sh
@@ -0,0 +1,1 @@
+docker-machine create --driver digitalocean --digitalocean-access-token $TOKEN --digitalocean-size s-1vcpu-2gb-amd --digitalocean-image ubuntu-20-04-x64 --digitalocean-region sgp1 --engine-install-url "https://releases.rancher.com/install-docker/19.03.9.sh" mydocker
