d1b38842538eae44b0d2131c75c10136e73dffdf
zyuezheng <zyuezheng@gmail.com>
2021-11-11 10:08:44 +0800

    modified dnsutils

--- utils/dnsutils.sh
+++ utils/dnsutils.sh
@@ -1,1 +1,2 @@
-kubectl run dnsutils --image=gcr.io/kubernetes-e2e-test-images/dnsutils:1.3 --restart=Never --command sleep 10000000
+kubectl run dnsutils --image=gcr.io/kubernetes-e2e-test-images/dnsutils:1.3 --restart=Never --command sleep 10000000  
+#-oyaml --dry-run=client
